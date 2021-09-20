ebc9d5556912d693846ecf4787c8df9107e3e0b8
Inotay Krisztián <krisztian.inotay@gmail.com>
2021-09-20 16:05:05 +0200

    Update example_kubernetes_executor.py

--- example_kubernetes_executor.py
+++ example_kubernetes_executor.py
@@ -69,14 +69,14 @@ with DAG(
     one_task = PythonOperator(
         task_id="one_task",
         python_callable=print_stuff,
-        executor_config={"KubernetesExecutor": {"image": "airflow/ci:latest"}},
+        executor_config={"KubernetesExecutor": {"image": "bash"}},
     )
 
     # Use the zip binary, which is only found in this special docker image
     two_task = PythonOperator(
         task_id="two_task",
         python_callable=assert_zip_binary,
-        executor_config={"KubernetesExecutor": {"image": "airflow/ci_zip:latest"}},
+        executor_config={"KubernetesExecutor": {"image": "bash"}},
     )
 
     # Limit resources on this operator/task with node affinity & tolerations
