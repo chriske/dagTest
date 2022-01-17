04904aee5a9add4fce647e5d3fd120e3f6e73116
Inotay Krisztián <krisztian.inotay@gmail.com>
2022-01-17 15:16:26 +0100

    Update 00-demo.py

--- 00-demo.py
+++ 00-demo.py
@@ -74,7 +74,7 @@ with DAG(
                         )
                     ],
                     volumes=[
-                            k8s.V1Volume(name="dag-temp", empty_dir="dag-temp"),
+                            k8s.V1Volume(name="dag-temp", persistent_volume_claim="dag-temp"),
                     ],
                     service_account_name="dag-test-runner"
                 )
@@ -96,7 +96,7 @@ with DAG(
                         )
                     ],
                     volumes=[
-                            k8s.V1Volume(name="dag-temp", empty_dir="dag-temp"),
+                            k8s.V1Volume(name="dag-temp", persistent_volume_claim="dag-temp"),
                     ],
                     service_account_name="dag-test-runner"
                 )
@@ -118,7 +118,7 @@ with DAG(
                         )
                     ],
                     volumes=[
-                            k8s.V1Volume(name="dag-temp", empty_dir="dag-temp"),
+                            k8s.V1Volume(name="dag-temp", persistent_volume_claim="dag-temp"),
                     ],
                     service_account_name="dag-test-runner"
                 )
